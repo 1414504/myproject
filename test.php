4c4e66439062211319fb8fd931a12867d097545d
1414504 <1414504@rgu.ac.uk>
2016-08-17 10:48:57 +0100

    Initial commit

---
+++ test.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+
+    <title>Shahid Baig</title>
+</head>
+
+<body>
+
+<div class="container">
+    <div class = "row">
+        <div class="col-md-3" style="height: 140px; background: "grey">Shahid Baig
+
+        </div>
+        <div class="col-lg-9" style="height: 140px; background: "lightgray"> My First Page
+
+        </div>
+
+    </div>
+<div class="row">
+    <div class="clo-md-4" style="min-height: 400px">
+        <a href="?cat=windows" style="text-align: left"; class="btn btn-default form-control">windows</a>
+        <a href="?cat=android" style="text-align: left" class="btn btn-default form-control">android</a>
+        <a href="?cat=ios" style="text-align: left" class="btn btn-default form-control">ios</a>
+
+    </div>
+
+</div>
+
+
+</div>
+
+</body>
+
+
+</html>
